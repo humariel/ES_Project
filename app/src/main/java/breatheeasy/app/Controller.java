ec93e01093f2103d261eedee22cddcdabeb89164
humariel <hum4r13l@gmail.com>
2020-04-15 22:49:20 +0100

    jenkins test

---
+++ app/src/main/java/breatheeasy/app/Controller.java
@@ -0,0 +1,12 @@
+package breatheeasy.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/quote")
+    public Quote getQuote(){
+        return ScheduledTasks.getQuote();
+    }
+}
