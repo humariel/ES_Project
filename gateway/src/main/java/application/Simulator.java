9f0e43b4d7dc30f1e175820155f023e4b739e98f
humariel <hum4r13l@gmail.com>
2020-04-30 18:07:19 +0100

    much better simulator. uncooment frontend in docker-compose to run everything with it

--- gateway/src/main/java/application/Simulator.java
+++ gateway/src/main/java/application/Simulator.java
@@ -1,144 +1,184 @@
 package application;
 
-import java.lang.Math;
+import java.util.Random;
 
 public class Simulator {
 
-    private final double T_RANDOM_VARIATION = 0.001;
-    private final double SECONDS_IN_DAY = 86400;
-    //how many seconds between simulations
-    private final double REQUEST_RATE = 0.1;
-    private final double T_INCREMENT = (1/SECONDS_IN_DAY) * REQUEST_RATE;
+    private String id;
+    private Location location;
+    private Random random;
 
-    //temperature
-    private final double TEMPERATURE_MIN = 5;
-    private final double TEMPERATURE_MAX = 40;
-    private double TEMPERATURE_T = 0;
+    //temperature    
+    private double TEMPERATURE;
+    private final double TEMPERATURE_AVG = 25;
+    private final double TEMPERATURE_STD = 20;
     //humidity
-    private final double HUMIDITY_MIN = 20;
-    private final double HUMIDITY_MAX = 80;
-    private double  HUMIDITY_T = 0.1;
+    private double  HUMIDITY;
+    private final double HUMIDITY_AVG = 30;
+    private final double HUMIDITY_STD = 10;
     //pressure
-    private final double PRESSURE_MIN = 5;
-    private final double PRESSURE_MAX = 40;
-    private double PRESSURE_T = 0.2;
+    private double PRESSURE;
+    private final double PRESSURE_AVG = 100;
+    private final double PRESSURE_STD = 200;
     //precipitation intensity
-    private final double PRECIPITATION_MIN = 20;
-    private final double PRECIPITATION_MAX = 80;
-    private double PRECIPITATION_T = 0.3;
+    private double PRECIPITATION;
+    private final double PRECIPITATION_AVG = 15;
+    private final double PRECIPITATION_STD = 5;
 
     //ozone
-    private final double O3_MIN = 0;
-    private final double O3_MAX = 100;
-    private double O3_T = 0.4;
+    private double O3;
+    private final double O3_AVG = 25;
+    private final double O3_STD = 20;
     //carbon monoxide
-    private final double CO_MIN = 20;
-    private final double CO_MAX = 80;
-    private double CO_T = 0.5;
+    private double CO;
+    private final double CO_AVG = 100;
+    private final double CO_STD = 150;
     //nitrogen dioxide
-    private final double NO2_MIN = 20;
-    private final double NO2_MAX = 80;
-    private double NO2_T = 0.6;
+    private double NO2;
+    private final double NO2_AVG = 15;
+    private final double NO2_STD = 15;
     //inhalable particulate matter (<10µm)
-    private final double PM10_MIN = 0;
-    private final double PM10_Max = 100;
-    private double PM10_T = 0.7;
+    private double PM10;
+    private final double PM10_AVG = 20;
+    private final double PM10_STD = 10;
     //inhalable particulate matter (<25µm)
-    private final double PM25_MIN = 0;
-    private final double PM25_MAX = 100;
-    private double PM25_T = 0.8;
+    private double PM25;
+    private final double PM25_AVG = 20;
+    private final double PM25_STD = 10;
     //sulfur dioxide
-    private final double SO2_MIN = 20;
-    private final double SO2_MAX = 80;
-    private double SO2_T = 0.9;
+    private double SO2;
+    private final double SO2_AVG = 5;
+    private final double SO2_STD = 2;
 
-    public Simulator() {
+    public Simulator(String id, Location location) {
+        this.random = new Random();
 
+        this.id =  id;
+        this.location = location;
+
+        this.TEMPERATURE = gaussian(this.TEMPERATURE_STD, this.TEMPERATURE_AVG);
+        this.HUMIDITY = gaussian(this.HUMIDITY_STD, this.HUMIDITY_AVG);
+        this.PRESSURE = gaussian(this.PRESSURE_STD, this.PRESSURE_AVG);
+        this.PRECIPITATION = gaussian(this.PRECIPITATION_STD, this.PRECIPITATION_AVG);
+
+        this.O3 = gaussian(this.O3_STD, this.O3_AVG);
+        this.CO = gaussian(this.CO_STD, this.CO_AVG);
+        this.NO2 = gaussian(this.NO2_STD, this.NO2_AVG);
+        this.PM10 = gaussian(this.PM10_STD, this.PM10_AVG);
+        this.PM25 = gaussian(this.PM25_STD, this.PM25_AVG);
+        this.SO2 = gaussian(this.SO2_STD, this.SO2_AVG);
     }
 
-    public Entity simulate(String id, Location location) {
-        Entity entity = new Entity();
+    public String getId() {
+        return this.id;
+    }
 
-        entity.setId(id);
-        entity.setLocation(location);
+    public void setId(String id) {
+        this.id = id;
+    }
 
-        entity.setTemperature(simulateValue(temperatureSin(), TEMPERATURE_MIN, TEMPERATURE_MAX));
-        entity.setHumidity(simulateValue(humiditySin(), HUMIDITY_MIN, HUMIDITY_MAX));
-        entity.setPressure(simulateValue(pressureSin(), PRESSURE_MIN, PRESSURE_MAX));
-        entity.setPrecipIntensity((int)simulateValue(precipitationSin(), PRECIPITATION_MIN, PRECIPITATION_MAX));
+    public Location getLocation() {
+        return this.location;
+    }
 
-        entity.setO3(simulateValue(o3Sin(), O3_MIN, O3_MAX));
-        entity.setCo(simulateValue(coSin(), CO_MIN, CO_MAX));
-        entity.setNo2(simulateValue(no2Sin(), NO2_MIN, NO2_MAX));
-        entity.setPm10(simulateValue(pm10Sin(), PM10_MIN, PM10_Max));
-        entity.setPm25(simulateValue(pm25Sin(), PM25_MIN, PM25_MAX));
-        entity.setSo2(simulateValue(so2Sin(), SO2_MIN, SO2_MAX));
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    private double gaussian(double std, double mean) {
+        return random.nextGaussian()*std + mean;
+    }
+
+    public Value simulate() {
+        Value entity = new Value();
+
+        entity.setId(this.id);
+        entity.setLocation(this.location);
+
+        entity.setTemperature(nextTemperature(this.TEMPERATURE_STD, this.TEMPERATURE_AVG));
+        entity.setHumidity(nextHumidity(this.HUMIDITY_STD, this.HUMIDITY_AVG));
+        entity.setPressure(nextPressure(this.PRESSURE_STD, this.PRESSURE_AVG));
+        entity.setPrecipIntensity((int) nextPrecipitation(this.PRECIPITATION_STD, this.PRECIPITATION_AVG));
+
+        entity.setO3(nextO3(this.O3_STD, this.O3_AVG));
+        entity.setCo(nextCo(this.CO_STD, this.CO_AVG));
+        entity.setNo2(nextNo2(this.NO2_STD, this.NO2_AVG));
+        entity.setPm10(nextPm10(this.PM10_STD, this.PM10_AVG));
+        entity.setPm25(nextPm25(this.PM25_STD, this.PM25_AVG));
+        entity.setSo2(nextSo2(this.SO2_STD, this.SO2_AVG));
 
         return entity;
     }
 
-    private double simulateValue(double sin, double min, double max) {
-        return sin*(max-min) + min;
+    private double nextTemperature(double std, double mean) {
+        this.TEMPERATURE += random.nextGaussian();
+        if(this.TEMPERATURE < 0) this.TEMPERATURE = 0;
+        else if(this.TEMPERATURE > mean + std) this.TEMPERATURE = mean + std;
+        return this.TEMPERATURE;
     }
 
-    private double temperatureSin() {
-        double res = Math.sin(TEMPERATURE_T);
-        TEMPERATURE_T += T_INCREMENT + Math.random()*T_RANDOM_VARIATION;
-        return res;
+    private double nextHumidity(double std, double mean) {
+        this.HUMIDITY += random.nextGaussian();
+        if(this.HUMIDITY < 0) this.HUMIDITY = 0;
+        else if(this.HUMIDITY >  mean + std) this.HUMIDITY = mean + std;
+        return this.HUMIDITY;
     }
 
-    private double humiditySin() {
-        double res = Math.sin(HUMIDITY_T);
-        HUMIDITY_T += T_INCREMENT + Math.random()*T_RANDOM_VARIATION;
-        return res;
+    private double nextPressure(double std, double mean) {
+        this.PRESSURE += random.nextGaussian();
+        if(this.PRESSURE < 0) this.PRESSURE = 0;
+        else if(this.PRESSURE > mean + std) this.PRESSURE = mean + std;
+        return this.PRESSURE;
     }
 
-    private double pressureSin() {
-        double res = Math.sin(PRESSURE_T);
-        PRESSURE_T += T_INCREMENT + Math.random()*T_RANDOM_VARIATION;
-        return res;
+    private double nextPrecipitation(double std, double mean) {
+        this.PRECIPITATION += random.nextGaussian();
+        if(this.PRECIPITATION < 0) this.PRECIPITATION = 0;
+        else if(this.PRECIPITATION > mean + std) this.PRECIPITATION = mean + std;
+        return this.PRECIPITATION;
     }
 
-    private double precipitationSin() {
-        double res = Math.sin(PRECIPITATION_T);
-        PRECIPITATION_T += T_INCREMENT + Math.random()*T_RANDOM_VARIATION;
-        return res;
+    private double nextO3(double std, double mean) {
+        this.O3 += random.nextGaussian();
+        if(this.O3 < 0) this.O3 = 0;
+        else if(this.O3 > mean + std) this.O3 = mean + std;
+        return this.O3;
     }
 
-    private double o3Sin() {
-        double res = Math.sin(O3_T);
-        O3_T += T_INCREMENT + Math.random()*T_RANDOM_VARIATION;
-        return res;
+    private double nextCo(double std, double mean) {
+        this.CO += random.nextGaussian();
+        if(this.CO < 0) this.CO = 0;
+        else if(this.CO > mean + std) this.CO = mean + std;
+        return this.CO;
     }
 
-    private double coSin() {
-        double res = Math.sin(CO_T);
-        CO_T += T_INCREMENT + Math.random()*T_RANDOM_VARIATION;
-        return res;
+    private double nextNo2(double std, double mean) {
+        this.NO2 += random.nextGaussian();
+        if(this.NO2 < 0) this.NO2 = 0;
+        else if(this.NO2 > mean + std) this.NO2 = mean + std;
+        return this.NO2;
     }
 
-    private double no2Sin() {
-        double res = Math.sin(NO2_T);
-        NO2_T += T_INCREMENT + Math.random()*T_RANDOM_VARIATION;
-        return res;
+    private double nextPm10(double std, double mean) {
+        this.PM10 += random.nextGaussian();
+        if(this.PM10 < 0) this.PM10 = 0;
+        else if(this.PM10 > mean + std) this.PM10 = mean + std;
+        return this.PM10;
     }
 
-    private double pm10Sin() {
-        double res = Math.sin(PM10_T);
-        PM10_T += T_INCREMENT + Math.random()*T_RANDOM_VARIATION;
-        return res;
+    private double nextPm25(double std, double mean) {
+        this.PM25 += random.nextGaussian();
+        if(this.PM25 < 0) this.PM25 = 0;
+        else if(this.PM25 > mean + std) this.PM25 = mean + std;
+        return this.PM25;
     }
 
-    private double pm25Sin() {
-        double res = Math.sin(PM25_T);
-        PM25_T += T_INCREMENT + Math.random()*T_RANDOM_VARIATION;
-        return res;
-    }
+    private double nextSo2(double std, double mean) {
+        this.SO2 += random.nextGaussian();
+        if(this.SO2 < 0) this.SO2 = 0;
+        else if(this.SO2 > mean + std) this.SO2 = mean + std;
+        return this.SO2;
 
-    private double so2Sin() {
-        double res = Math.sin(SO2_T);
-        SO2_T += T_INCREMENT + Math.random()*T_RANDOM_VARIATION;
-        return res;
     }
 
 }
