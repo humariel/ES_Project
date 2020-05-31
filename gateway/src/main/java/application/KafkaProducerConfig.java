641db2e7b791cae46e4fb95935a3c3d09f7d0dcb
enamak <gbmvitor@gmail.com>
2020-05-31 19:43:58 +0100

    Alarm server  added

--- gateway/src/main/java/application/KafkaProducerConfig.java
+++ gateway/src/main/java/application/KafkaProducerConfig.java
@@ -1,10 +1,14 @@
 package application;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
@@ -43,12 +47,43 @@ public class KafkaProducerConfig {
         return new KafkaTemplate<>(producerFactoryValue());
     }
 
-
     /* @Bean
     public KafkaTemplate<String, Trigger> kafkaTemplateTrigger() {
         return new KafkaTemplate<>(producerFactoryTrigger());
     
     } */
+
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic1() {
+        return new NewTopic("entity", 5, (short) 2);
+    }
+
+    @Bean
+    public NewTopic topic2() {
+        return new NewTopic("value", 5, (short) 2);
+    }
+
+    @Bean
+    public NewTopic topic3() {
+        return new NewTopic("parish", 5, (short) 2);
+    }
+
+    @Bean
+    public NewTopic topic4() {
+        return new NewTopic("alarm", 5, (short) 2);
+    }
+
+    @Bean
+    public NewTopic topic5() {
+        return new NewTopic("trigger", 5, (short) 2);
+    }
 
 
 }
